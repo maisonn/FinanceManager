31789084635f8824ce03db256b5096018116740d
maisonn <37782167+maisonn@users.noreply.github.com>
2021-01-26 16:42:11 +0200

    1

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,36 +1,10 @@
 import flatpickr from "flatpickr"
 
 document.addEventListener('turbolinks:load', function() {
-    flatpickr('.flatpickr', {
-      dateFormat: "Y-m-d",
-    });
-})
+  flatpickr('.flatpickr', {
+    altInput: true,
+    dateFormat: "Y-m-d",
+  });
+}) 
 
-const startDateInput = document.getElementById('report_start_date');
-const endDateInput = document.getElementById('report_end_date');
-
-
-if (startDateInput) {
-const unavailableDates = JSON.parse(document.querySelector('#flat-booking-dates').dataset.unavailable)
-endDateInput.disabled = true
-
-flatpickr(startDateInput, {
-  minDate: "today",
-  disable: unavailableDates,
-  dateFormat: "Y-m-d",
-});
-
-console.log('im in the file')
-
-startDateInput.addEventListener("change", (e) => {
-  if (startDateInput != "") {
-    endDateInput.disabled = false
-  }
-  flatpickr(endDateInput, {
-    minDate: e.target.value,
-    disable: unavailableDates,
-    dateFormat: "Y-m-d"
-    });
-  })
-};
 
