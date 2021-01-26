fd2e159fb994a469dabdd08319475739bf85780e
maisonn <37782167+maisonn@users.noreply.github.com>
2021-01-26 02:48:55 +0200

    Everything Is Working

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,10 +1,11 @@
 class ReportsController < ApplicationController
 
+
   def index
     @expenses = Expense.all
+    @start_date = params[:start_date]
+    @end_date = params[:end_date]
   end
-
-  
 
 
 
