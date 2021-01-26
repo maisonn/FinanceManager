569555629fad390dea6856cc7a1d2649874beec2
maisonn <37782167+maisonn@users.noreply.github.com>
2021-01-26 22:56:49 +0200

    1

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,13 +1,8 @@
 class ReportsController < ApplicationController
-
-
   def index
     @expenses = Expense.all
     @incomes = Income.all
     @start_date = params[:start_date]
     @end_date = params[:end_date]
   end
-
-
-
 end
