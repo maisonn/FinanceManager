5ee67579bd437b309cf50ade3fd3f2385e09af01
maisonn <37782167+maisonn@users.noreply.github.com>
2021-01-27 16:14:03 +0200

    Done :)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   resources :incomes
   resources :reports
   resources :categories
   resources :expenses
-  root 'reports#index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
