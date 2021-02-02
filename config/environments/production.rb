56975f8c07fd485689f427d1b9cc58194e6058b7
maisonn <37782167+maisonn@users.noreply.github.com>
2021-02-03 00:17:28 +0200

    1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,6 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "ExpenseTracker_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = {:host => 'herokuapp.com'}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
