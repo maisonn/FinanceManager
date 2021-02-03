013689e24fc9be32d77df7fef42453a8069940ec
maisonn <37782167+maisonn@users.noreply.github.com>
2021-02-03 23:14:50 +0200

    1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :categories, dependent: :delete_all
   has_many :expenses, dependent: :delete_all
   has_many :incomes, dependent: :delete_all
